4d99e3ad3dea48e5ded4b69f6b7b0458af7880d6
CGro611 <CGro611@aucklanduni.ac.nz>
2025-09-08 12:49:06 +1200

    Updated main to call a collection to get the list of commands.

--- mainApp.js
+++ mainApp.js
@@ -1,7 +1,13 @@
-const {Client, Events, GatewayIntentBits} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Collection, Events, GatewayIntentBits, MessageFlags} = require('discord.js');
 const {token} = require('./config.json');
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+//Loads all the commands from the file.
+client.commands = new Collection();
+
 
 //Waits for the client to be ready then notifies the console.
 client.once(Events.ClientReady, readyClient => {
