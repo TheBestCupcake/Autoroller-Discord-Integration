abf1a3a8b3bef648fa6246415207f605f20db6d0
CGro611 <CGro611@aucklanduni.ac.nz>
2025-09-08 13:21:24 +1200

    Goes through all of the files in the commands folder and adds them to the client Collection.

--- mainApp.js
+++ mainApp.js
@@ -14,5 +14,31 @@ client.once(Events.ClientReady, readyClient => {
     console.log("Client Ready. Logged as ${readyClient.user.tag}");
 });
 
+//Gets the path to the commands file.
+const folderPath = path.join(__dirname, 'commands');
+//Returns an array of all folders inside the commands folder.
+const commandFolder = fs.readdirSync(folderPath);
+
+for(const folder of commandFolder){
+    //Goes through each folder and finds the files that end with .js.
+    const commandPath = path.join(folderPath, folder);
+    const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith(".js"));
+
+    for(const file of commandFiles){
+        //gets the command file path.
+        const filePath = path.join(commandPath, file);
+        const command = require(filePath);
+        //Sets a new command in the commands collection.
+        if('data' in command && 'execute' in command){
+            client.commands.set(command.data.name, command);
+        }
+        else{
+            console.log("[Warning] File does not have the 'data' or 'execute' properties.")
+        }
+    }
+}
+
+
+
 //Logs the client in.
 client.login(token);
