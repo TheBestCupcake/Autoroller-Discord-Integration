e42c2e7b68b804100bace7925f1e4fc9ea0caee4
CGro611 <CGro611@aucklanduni.ac.nz>
2025-09-08 14:59:24 +1200

    Added a post listeniner to the bot so it takes a JSON as input and outputs a message in the channel.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {token} = require('./config.json');
 const {Client, Collection, Events, GatewayIntentBits, MessageFlags} = require('discord.js');
+const bodyParser = require('body-parser');
+const express = require('express');
 
 const client = new Client({
     intents: [
@@ -10,10 +12,36 @@ const client = new Client({
 
 client.login(token);
 
-client.on("messageCreate", async (message) => {
-    console.log(message)
-
-    if(!message?.author.bot){
-        message.channelId.send('yes');
+//Send message function.
+const sendMessage = (channelID, message) => {
+    const channel = client.channels.cache.get(channelID);
+    if(channel){
+        channel.send(message);
     }
+    else{
+        console.error("Channel not found.");
+    }
+};
+
+
+//Sets up the express server.
+const app = express();
+app.use(bodyParser.json());
+
+//Sends a message with the rolled values. Takes a JSON of the channel id and the string as inputs. 
+app.post('/roll', (request, res) => {
+    const {channelID, message} = request.body;
+    if(!channelID || !message) {
+        return res.status(400).send({error: 'Missing values'});
+    }
+
+    sendMessage(channelID, message);
+
+    res.send({ status: 'Message sent successfully' });
+
+})
+
+//Listens to this local server.
+app.listen(5000, () => {
+    console.log('Server listening on http://localhost:5000');
 });
