0826802d1ef731ad12fc5ba0a888f814aa853005
CGro611 <CGro611@aucklanduni.ac.nz>
2025-10-08 20:32:54 +1300

    Rewrote index file.

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
+
+
+const {Client, Events, GatewayIntentBits} = require('discord.js');
 const {token} = require('./config.json');
-const {Client, Collection, Events, GatewayIntentBits, MessageFlags} = require('discord.js');
-const bodyParser = require('body-parser');
-const express = require('express');
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-    ],
+//New client instance.
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+//Check if client is running properly.
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Logged in as ${readyClient.user.tag}`);
 });
-
+//Logs client in as the bot.
 client.login(token);
 
-//Send message function.
-const sendMessage = (channelID, message) => {
-    const channel = client.channels.cache.get(channelID);
-    if(channel){
-        channel.send(message);
-    }
-    else{
-        console.error("Channel not found.");
-    }
-};
-
-
-//Sets up the express server.
-const app = express();
-app.use(bodyParser.json());
-
-//Sends a message with the rolled values. Takes a JSON of the channel id and the string as inputs. 
-app.post('/roll', (request, res) => {
-    const {channelID, message} = request.body;
-    if(!channelID || !message) {
-        return res.status(400).send({error: 'Missing values'});
-    }
-
-    sendMessage(channelID, message);
-
-    res.send({ status: 'Message sent successfully' });
-
-})
-
-//Listens to this local server.
-app.listen(5000, () => {
-    console.log('Server listening on http://localhost:5000');
-});
