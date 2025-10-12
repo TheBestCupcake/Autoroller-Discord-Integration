d3d2c5b878dbaab17dd85507884219a5ce6f82f3
CGro611 <CGro611@aucklanduni.ac.nz>
2025-10-13 10:32:39 +1300

    container set as response instead. testing for full implementation of autoroller feature.

--- commands/utilities/roll.js
+++ commands/utilities/roll.js
@@ -1,8 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {ActionRowBuilder, ButtonBuilder, ButtonStyle, ContainerBuilder, UserSelectMenuBuilder, MessageFlags, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder} = require('discord.js');
+const { response } = require('express');
 
 module.exports = {
     data: new SlashCommandBuilder().setName('roll').setDescription('Rolls your dice.'),
-    async execute(interaction) {
-        await interaction.reply("Rolled.");
-    },
+	async execute(interaction) {
+
+		const container = new ContainerBuilder()
+		.setAccentColor(0x0099ff)
+		.addTextDisplayComponents((textDisplay) =>
+			textDisplay.setContent(
+				"Text Inside the text container."
+			),
+		)
+		.addActionRowComponents((actionRow) =>
+			actionRow.setComponents(new UserSelectMenuBuilder().setCustomId('exampleSelect').setPlaceholder('Select users')),
+		)
+		.addSeparatorComponents((separator) => separator)
+
+
+
+		await interaction.reply({
+			components: [container],
+			flags: MessageFlags.IsComponentsV2,
+		});
+	},
 };
